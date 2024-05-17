7881a2d8e866b3d1d29866532a517aa97e835917
andikashln <andikashalihin01@gmail.com>
2024-05-17 19:16:27 +0700

    first commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="child-component">
+      <h2>Child Component</h2>
+      <form @submit.prevent="sendData">
+        <label for="childData">Enter Data:</label>
+        <input v-model="childData" id="childData" type="text" />
+        <button type="submit">Submit</button>
+      </form>
+      <div class="slot-container">
+        <slot :slotData="childData"></slot>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ChildComponent',
+    data() {
+      return {
+        childData: ''
+      };
+    },
+    methods: {
+      sendData() {
+        this.$emit('data-updated', this.childData);
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .child-component {
+    border: 1px solid #ccc;
+    padding: 20px;
+    margin: 10px;
+  }
+  .slot-container {
+    border: 1px dashed #aaa;
+    padding: 10px;
+  }
+  form {
+    margin-bottom: 10px;
+  }
+  </style>
+  
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+      <h1>Parent Component</h1>
+      <ChildComponent @data-updated="updateDataFromChild">
+        <template v-slot:default="slotProps">
+          <p>Data from slot: {{ slotProps.slotData }}</p>
+        </template>
+      </ChildComponent>
+      <p>Data from child: {{ dataFromChild }}</p>
+      <button @click="clearData">Clear Data</button>
+      <button @click="setDefaultData">Set Default Data</button>
+    </div>
+  </template>
+  
+  <script>
+  import ChildComponent from './ChildComponent.vue';
+  
+  export default {
+    components: {
+      ChildComponent
+    },
+    data() {
+      return {
+        dataFromChild: ''
+      };
+    },
+    methods: {
+      updateDataFromChild(data) {
+        this.dataFromChild = data;
+      },
+      clearData() {
+        this.dataFromChild = '';
+        this.$refs.childComponent.childData = '';
+      },
+      setDefaultData() {
+        this.dataFromChild = 'Default Data';
+        this.$refs.childComponent.childData = 'Default Data';
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  button {
+    margin: 5px;
+  }
+  </style>
+  
